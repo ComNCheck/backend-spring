b204357439da469f556ccad2734e5dc337c3fd1e
SHIMHUN <shimhun88@naver.com>
2025-08-12 22:09:43 +0900

    feat: 임시 행사 관련 request, response DTO 구현(#101)

---
+++ src/main/java/com/ComNCheck/ComNCheck/domain/majorEvent/model/dto/response/TempEventResponseDTO.java
@@ -0,0 +1,48 @@
+package com.ComNCheck.ComNCheck.domain.majorEvent.model.dto.response;
+
+import com.ComNCheck.ComNCheck.domain.majorEvent.model.entity.TempMajorEvent;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Getter
+@Builder
+public class TempEventResponseDTO {
+    private Long tempEventId;
+    private String eventName;
+    private String category;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private LocalTime time;
+    private String location;
+    private String notice;
+    private String googleFormLink;
+    private List<String> cardNewsImageUrls;
+
+    public static TempEventResponseDTO from(TempMajorEvent event) {
+        return TempEventResponseDTO.builder()
+                .tempEventId(event.getId())
+                .eventName(event.getEventName())
+                .category(event.getCategory().name()) // Enum -> String
+                .startDate(event.getStartDate())
+                .endDate(event.getEndDate())
+                .time(event.getTime())
+                .location(event.getLocation())
+                .notice(event.getNotice())
+                .googleFormLink(event.getGoogleFormLink())
+                .cardNewsImageUrls(event.getCardNewsImageUrls())
+                .build();
+    }
+
+    @Builder
+    public static class Fix{
+        private Long Id;
+        private String eventName;
+    }
+}
