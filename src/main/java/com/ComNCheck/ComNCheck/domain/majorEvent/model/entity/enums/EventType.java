ddce673aa37bf795074f04b7e7705032d2e3d216
SHIMHUN <shimhun88@naver.com>
2025-08-12 22:29:39 +0900

    feat: 행사 등록시 필요한 카테고리 정의(#101)
    FilterCategory
      - 달력에서 필터링 목적으로 사용
      - 넓은 범위의 행사 그룹을 정의
    
    EventType
      - DB에 저장될 실제 행사 카테고리
      - 구체적인 행사 카테고리 정의

---
+++ src/main/java/com/ComNCheck/ComNCheck/domain/majorEvent/model/entity/enums/EventType.java
@@ -0,0 +1,42 @@
+package com.ComNCheck.ComNCheck.domain.majorEvent.model.entity.enums;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public enum EventType {
+
+    FRESHMAN_ORIENTATION("새내기 배움터", FilterCategory.FRESHMAN_ORIENTATION),
+    FIRST_SEMESTER_OPENING_MEETING("1학기 개강총회", FilterCategory.MEETING),
+    FIRST_SEMESTER_CLOSING_MEETING("1학기 종강총회", FilterCategory.MEETING),
+    SECOND_SEMESTER_OPENING_MEETING("2학기 개강총회", FilterCategory.MEETING),
+    SECOND_SEMESTER_CLOSING_MEETING("2학기 종강총회", FilterCategory.MEETING),
+    FACE_TO_FACE_MEETING("대면식", FilterCategory.FACE_TO_FACE_MEETING),
+    FIRST_SEMESTER_MIDTERM_SNACK("1학기 중간고사 간식행사", FilterCategory.SNACK_EVENT),
+    FIRST_SEMESTER_FINAL_SNACK("1학기 기말고사 간식행사", FilterCategory.SNACK_EVENT),
+    SECOND_SEMESTER_MIDTERM_SNACK("2학기 중간고사 간식행사", FilterCategory.SNACK_EVENT),
+    SECOND_SEMESTER_FINAL_SNACK("2학기 기말고사 간식행사", FilterCategory.SNACK_EVENT),
+    MT("MT", FilterCategory.MT),
+    KICK_OFF("해오름식", FilterCategory.KICK_OFF),
+    COLLEGE_SPORTS_DAY("공대 체전", FilterCategory.SPORTS_DAY),
+    UNIVERSITY_SPORTS_DAY("왕산 체전", FilterCategory.SPORTS_DAY),
+    FESTIVAL("축제", FilterCategory.FESTIVAL),
+    HOMECOMING_DAY("홈커밍 데이", FilterCategory.HOME_COMING_DAY);
+
+    private final String displayName;
+    private final FilterCategory filterCategory;
+
+    EventType(String displayName, FilterCategory filterCategory) {
+        this.displayName = displayName;
+        this.filterCategory = filterCategory;
+    }
+
+    public static List<EventType> getByFilterCategory(FilterCategory category) {
+        return Arrays.stream(EventType.values())
+                .filter(eventType -> eventType.getFilterCategory() == category)
+                .collect(Collectors.toList());
+    }
+}
