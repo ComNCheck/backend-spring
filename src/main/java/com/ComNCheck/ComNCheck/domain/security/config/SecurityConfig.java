f642eef4e8b265023b4af292967a5747d6357221
SHIMHUN <shimhun88@naver.com>
2025-09-15 15:29:56 +0900

    chore: 주석 삭제

--- src/main/java/com/ComNCheck/ComNCheck/domain/security/config/SecurityConfig.java
+++ src/main/java/com/ComNCheck/ComNCheck/domain/security/config/SecurityConfig.java
@@ -69,7 +69,6 @@ public class SecurityConfig {
                         "/v3/api-docs/**",
                         "/swagger-resources/**",
                         "/webjars/**",
-//                        "/api/v1/**"
                         "/api/v1/member/login"
                 ).permitAll()
 
