4ddf30a6017ec3d5a9f1fd3bbd9fbc1ca51c25b2
SHIMHUN <shimhun88@naver.com>
2025-09-19 22:16:45 +0900

    fix: @Repository 어노테이션 누락 확인 후 수정

--- src/main/java/com/ComNCheck/ComNCheck/domain/majorEvent/repository/TempMajorEventRepository.java
+++ src/main/java/com/ComNCheck/ComNCheck/domain/majorEvent/repository/TempMajorEventRepository.java
@@ -4,10 +4,12 @@ import com.ComNCheck.ComNCheck.domain.majorEvent.model.entity.TempMajorEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface TempMajorEventRepository extends JpaRepository<TempMajorEvent, Long> {
     @Query("SELECT tme FROM TempMajorEvent tme WHERE " +
             "tme.startDate <= :monthEnd AND (tme.endDate IS NULL OR tme.endDate >= :monthStart)")
