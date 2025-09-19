4d69cf4996c34c1eea505650bbc27e8f363d4e72
SHIMHUN <shimhun88@naver.com>
2025-09-19 22:22:33 +0900

    feat: hostCategory로 행사 조회하는 메서드 추가

--- src/main/java/com/ComNCheck/ComNCheck/domain/majorEvent/repository/MajorEventRepository.java
+++ src/main/java/com/ComNCheck/ComNCheck/domain/majorEvent/repository/MajorEventRepository.java
@@ -6,11 +6,14 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
+import com.ComNCheck.ComNCheck.domain.majorEvent.model.entity.enums.HostType;
 import com.ComNCheck.ComNCheck.domain.majorEvent.repository.querydsl.MajorEventRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MajorEventRepository extends JpaRepository<MajorEvent, Long>, MajorEventRepositoryCustom {
 //    @Query("""
 //        SELECT e
@@ -30,4 +33,16 @@ public interface MajorEventRepository extends JpaRepository<MajorEvent, Long>, M
     @Query("SELECT me FROM MajorEvent me WHERE " +
             "me.startDate <= :monthEnd AND (me.endDate IS NULL OR me.endDate >= :monthStart)")
     List<MajorEvent> findByDateRange(@Param("monthStart") LocalDate monthStart, @Param("monthEnd") LocalDate monthEnd);
+
+    @Query("""
+        SELECT e FROM MajorEvent e
+        WHERE e.hostType = :hostType
+          AND (e.startDate > :today OR (e.startDate = :today AND e.time >= :now))
+        ORDER BY e.startDate DESC, e.time DESC
+    """)
+    List<MajorEvent> findNotPassedEventsByHostCategory(
+            @Param("today") LocalDate today,
+            @Param("now") LocalTime now,
+            @Param("hostType") HostType hostType
+    );
 }
